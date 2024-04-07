9e1d809945bfde5b102d39e9d042323672bd8e6b
Jose David Lagos Morales <consultor_dgpmacdf32@mef.gob.pe>
2024-04-07 00:51:43 -0500

    cliente de config server y apigateway v2

--- src/main/java/pe/com/cayetano/see/comunes/SeeComunesApplication.java
+++ src/main/java/pe/com/cayetano/see/comunes/SeeComunesApplication.java
@@ -3,6 +3,7 @@ package pe.com.cayetano.see.comunes;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @OpenAPIDefinition
